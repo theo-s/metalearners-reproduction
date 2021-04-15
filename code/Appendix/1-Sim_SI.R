9ea5b952a0dd88fdc75dc62b01da8d8d26a03e42
Theo <theo_s@berkeley.edu>
2021-04-15 14:58:15 -0400

    Add code for Simulations 1 and 6

--- code/Appendix/1-Sim_SI.R
+++ code/Appendix/1-Sim_SI.R
@@ -1,1 +1,118 @@
 # Reproduces Appendix Simulation SI 1 (unbalanced treatment assignment).
+library(causalToolbox)
+library(npcausal)
+library(ggplot2)
+library(dplyr)
+library(reshape)
+library(ggrepel)
+library(viridis)
+
+#?simulate_causal_experiment
+results <- data.frame(N = c(1000, 2000, 5000, 10000, 20000))
+results$XRF <- NA
+results$XBART <- NA
+results$SRF <- NA
+results$SBART <- NA
+results$TRF <- NA
+results$TBART <- NA
+
+for (n in c(1000, 2000, 5000, 10000, 20000)) {
+  complex_linear_experiment <- simulate_causal_experiment(ntrain = n,
+                                                          ntest = 1000,
+                                                          dim = 20,
+                                                          alpha = 0,
+                                                          pscore = "rct01",
+                                                          mu0 = "semiLinear",
+                                                          tau = "semiLinear")
+
+
+  feature_train <- complex_linear_experiment$feat_tr
+  w_train <- complex_linear_experiment$W_tr
+  yobs_train <- complex_linear_experiment$Yobs_tr
+
+  # Train the X Learner with BART and RF
+  print(paste0("Training XRF, N = ", n))
+  xl_rf <- X_RF(feat = feature_train, tr = w_train, yobs = yobs_train)
+  print(paste0("Training XBART, N = ", n))
+  xl_bart <- X_BART(feat = feature_train, tr = w_train, yobs = yobs_train)
+  print(paste0("Training SRF, N = ", n))
+  sl_rf <- S_RF(feat = feature_train, tr = w_train, yobs = yobs_train)
+  print(paste0("Training SBART, N = ", n))
+  sl_bart <- S_BART(feat = feature_train, tr = w_train, yobs = yobs_train)
+  print(paste0("Training TRF, N = ", n))
+  tl_rf <- T_RF(feat = feature_train, tr = w_train, yobs = yobs_train)
+  print(paste0("Training TBART, N = ", n))
+  tl_bart <- T_BART(feat = feature_train, tr = w_train, yobs = yobs_train)
+
+  # estimate the CATE
+  feature_test <- complex_linear_experiment$feat_te
+
+  cate_esti_xrf <- EstimateCate(xl_rf, feature_test)
+  cate_esti_xbart <- EstimateCate(xl_bart, feature_test)
+  cate_esti_srf <- EstimateCate(sl_rf, feature_test)
+  cate_esti_sbart <- EstimateCate(sl_bart, feature_test)
+  cate_esti_trf <- EstimateCate(tl_rf, feature_test)
+  cate_esti_tbart <- EstimateCate(tl_bart, feature_test)
+
+  # evaluate the performance
+  cate_true <- complex_linear_experiment$tau_te
+  results$XRF[which(results$N == n)] <- mean((cate_esti_xrf - cate_true)^2)
+  results$XBART[which(results$N == n)] <- mean((cate_esti_xbart - cate_true)^2)
+  results$SRF[which(results$N == n)] <- mean((cate_esti_srf - cate_true)^2)
+  results$SBART[which(results$N == n)] <- mean((cate_esti_sbart - cate_true)^2)
+  results$TRF[which(results$N == n)] <- mean((cate_esti_trf - cate_true)^2)
+  results$TBART[which(results$N == n)] <- mean((cate_esti_tbart - cate_true)^2)
+
+  # Save the intermediate results
+  write.csv(results,
+            file = "results/complex_linearEMSE.csv",
+            row.names = FALSE)
+
+  # Clean up the environment
+  rm(xl_rf, xl_bart, sl_rf, sl_bart, tl_bart, tl_rf)
+}
+
+min <- min(results %>%
+             melt(id = "N") %>%
+             select(value))
+
+max <- max(results %>%
+             melt(id = "N") %>%
+             select(value))
+
+results %>%
+  melt(id = "N") %>%
+  filter(substr(variable, 2,5) == "BART") %>%
+  dplyr::rename(Estimator = variable) %>%
+  dplyr::mutate(Estimator = plyr::revalue(Estimator, c("XBART" = "X Learner (BART)",
+                                                       "SBART" = "S Learner (BART)",
+                                                       "TBART" = "T Learner (BART)"))) %>%
+  ggplot(aes(x = N, y = value, color = Estimator))+
+  geom_line()+
+  geom_point()+
+  scale_y_log10(limits = c(1, max))+
+  theme_bw()+
+  scale_color_viridis_d()+
+  labs(x = "Training Size", y = "MSE")
+
+ggsave(filename = "figures/complex_linear_BART.pdf", height = 6, width = 6)
+
+results %>%
+  melt(id = "N") %>%
+  filter(substr(variable, 2,5) != "BART") %>%
+  dplyr::rename(Estimator = variable) %>%
+  dplyr::mutate(Estimator = plyr::revalue(Estimator, c("XRF" = "X Learner (RF)",
+                                                       "SRF" = "S Learner (RF)",
+                                                       "TRF" = "T Learner (RF)"))) %>%
+  ggplot(aes(x = N, y = value, color = Estimator))+
+  geom_line()+
+  geom_point()+
+  scale_y_log10(limits = c(1, max))+
+  theme_bw()+
+  scale_color_viridis_d()+
+  labs(x = "Training Size", y = "MSE")
+
+ggsave(filename = "figures/complex_linear_RF.pdf", height = 6, width = 6)
+
+
+
